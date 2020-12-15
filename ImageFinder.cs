27411c762cf11830e1110917c2b24f91f12bb461
Corentin-B <banteignie.c@gmail.com>
2020-12-15 17:54:36 +0100

    Recupération des images et convertion en bitmap

---
+++ ImageFinder.cs
@@ -0,0 +1,39 @@
+﻿using Dual_Image_Finder.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Dual_Image_Finder
+{
+    class ImageFinder
+    {
+        public List<InfoImage> GetImagesInFolder(string PathFolder)
+        {
+            string[] FileInFolder = Directory.GetFiles(PathFolder, "*.*g", SearchOption.AllDirectories);
+            List<InfoImage> listInfoImages = new List<InfoImage>();
+
+            foreach (string File in FileInFolder)
+            {
+                InfoImage infoImage = new InfoImage();
+                infoImage.Name = "";
+                infoImage.Path = File;
+
+                Image image = Image.FromFile(File);
+                Bitmap bitmapImage = new Bitmap(image);
+
+                MemoryStream ms = new MemoryStream();
+                bitmapImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                String imageBitmap = Convert.ToBase64String(ms.ToArray());
+                ms.Position = 0;
+
+                infoImage.Bitmap = imageBitmap;
+                infoImage.Deleted = false;
+
+                listInfoImages.Add(infoImage);
+            }
+            return listInfoImages;
+        }
+    }
+}

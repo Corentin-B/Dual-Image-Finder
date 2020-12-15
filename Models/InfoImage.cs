bfb25209f3b83099d4109e7d0f9c263ae2e959b6
Corentin-B <banteignie.c@gmail.com>
2020-12-15 15:00:46 +0100

    Model et supression

---
+++ Models/InfoImage.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Dual_Image_Finder.Models
+{
+    class InfoImage
+    {
+        string Name;
+        string Path;
+        Bitmap bitmap;
+        bool Deleted;
+    }
+}

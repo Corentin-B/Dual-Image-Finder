772d4e76c9381b3b0011ceae5fa893b818bce85d
Corentin-B <banteignie.c@gmail.com>
2020-12-15 13:46:22 +0100

    Interface terminée

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 
 namespace Dual_Image_Finder
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void button_targetFolder_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog_1.ShowDialog();
+            this.textBox_1.Text = this.folderBrowserDialog_1.SelectedPath;
+        }
+
+        private void button_ImgLeftNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_ImgLeftFolder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_ImgLeftDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_ImgRightNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_ImgRightFolder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_ImgRightDelete_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

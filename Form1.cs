24dc99d6677435a4f7d8354dc2f2d3d7d85ba614
Corentin-B <banteignie.c@gmail.com>
2020-12-16 11:58:35 +0100

    Ouverture de dossier et suppression

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,17 @@
 ﻿using Dual_Image_Finder.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Dual_Image_Finder
 {
     public partial class MainForm : Form
     {
+        private InfoImage leftInfoImage;
+        private InfoImage rightInfoImage;
+
         public MainForm()
         {
             InitializeComponent();
@@ -16,18 +21,23 @@ namespace Dual_Image_Finder
         {
             this.folderBrowserDialog_1.ShowDialog();
             this.textBox_1.Text = this.folderBrowserDialog_1.SelectedPath;
+
+            if (this.folderBrowserDialog_1.SelectedPath.Length != 0)
+            {
+                this.button_Start.Enabled = true;
+            }
         }
 
         private void button_Start_Click(object sender, EventArgs e)
         {
             //Call search method
+
+            //Create Thread
+
             ImageFinder imageFinder = new ImageFinder();
             List<InfoImage> maliste = imageFinder.GetImagesInFolder(this.folderBrowserDialog_1.SelectedPath);
-            
-            foreach(InfoImage unitem in maliste)
-            {
-                Console.WriteLine(unitem);
-            }
+            Comparator comparator = new Comparator();
+            comparator.ImageComparator(maliste);
         }
 
         private void button_ImgLeftNext_Click(object sender, EventArgs e)
@@ -38,11 +48,14 @@ namespace Dual_Image_Finder
         private void button_ImgLeftFolder_Click(object sender, EventArgs e)
         {
             //Open folder current image
+            openFolder(leftInfoImage.Path);
         }
 
         private void button_ImgLeftDelete_Click(object sender, EventArgs e)
         {
             //Delete Image
+            deleteFile(leftInfoImage.Path);
+            //Continue
         }
 
         private void button_ImgRightNext_Click(object sender, EventArgs e)
@@ -53,11 +66,48 @@ namespace Dual_Image_Finder
         private void button_ImgRightFolder_Click(object sender, EventArgs e)
         {
             //Open folder current image
+            openFolder(rightInfoImage.Path);
+            //Contine
         }
 
         private void button_ImgRightDelete_Click(object sender, EventArgs e)
         {
             //Delete Image
+            deleteFile(rightInfoImage.Path);
+            //Continue
+        }
+
+
+        private void openFolder(string folderPath)
+        {
+            try
+            {
+                Process.Start(Path.GetDirectoryName(folderPath));
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Erreur : Impossible d'ouvrir le dossier");
+            }
+        }
+
+        private void deleteFile(string filePath)
+        {
+            try
+            {
+                File.Delete(filePath);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Erreur : Accès non autorisé");
+            }
+            catch (PathTooLongException)
+            {
+                MessageBox.Show("Erreur : Chemin trop long");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Erreur : Fichier introuvable");
+            }
         }
     }
 }
